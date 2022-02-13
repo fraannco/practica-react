2a7657f603483d61b9d033f2ae4e36d0ccf0d222
francosilva25 <e.franco250299@gmail.com>
2022-02-12 21:31:01 -0500

    Todo List

---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,14 @@
+
+
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        // case 'add':
+
+        //     break;
+
+        default:
+            return state
+    }
+}
+
+export default todoReducer
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './components/08-useReducer/TodoApp';
 // import { Padre } from './components/07-tarea-memo/Padre';
-// // import CallbackHooks from './components/06-memos/CallbackHooks';
-// // import MemoHook from './components/06-memos/MemoHook';
-// // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
-// // import FocusScreen from './components/04-useRef/FocusScreen';
-// // import RealExampleRef from './components/04-useRef/RealExampleRef';
-// // import Layout from './components/05-useLayoutEffect/Layout';
-// // import Memorize from './components/06-memos/Memorize'
+// import CallbackHooks from './components/06-memos/CallbackHooks';
+// import MemoHook from './components/06-memos/MemoHook';
+// import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// import FocusScreen from './components/04-useRef/FocusScreen';
+// import RealExampleRef from './components/04-useRef/RealExampleRef';
+// import Layout from './components/05-useLayoutEffect/Layout';
+// import Memorize from './components/06-memos/Memorize'
 
-// ReactDOM.render(
-//   <Padre />,
-//   document.getElementById('root')
-// );
-
-import './components/08-useReducer/intro-reducer'
+ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('root')
+);
